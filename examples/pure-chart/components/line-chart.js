a8d9960b0e37174579c5065c616c2d05e561dced
fkhosrow <kh.farnaz@gmail.com>
2018-10-07 00:11:59 -0700

    Chart optimization:
    Cache computations for points that have been already drawn. The chart is shifted to the left by n places therefore, for every render, it's the existing data minus the first point plus n new points.

--- examples/pure-chart/components/line-chart.js
+++ examples/pure-chart/components/line-chart.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { View, TouchableWithoutFeedback, Text, Animated, Easing, ScrollView, StyleSheet } from 'react-native'
-import { Toast } from "@remobile/react-native-toast"
+import React from 'react';
+import { View, TouchableWithoutFeedback, Text, Animated, Easing, ScrollView, StyleSheet } from 'react-native';
+import { Toast } from "@remobile/react-native-toast";
 
 class LineChart extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {
-      data: this.props.data
-    };
+    this.state = { data: this.props.data };
+
+    this.cachedObjs = [];
 
     this.drawCoordinates = this.drawCoordinates.bind(this);
     this.drawCoordinate = this.drawCoordinate.bind(this);
@@ -19,37 +19,76 @@ class LineChart extends React.Component {
 
   componentWillReceiveProps (nextProps) {
     if (nextProps.data !== this.props.data) {
-      this.setState({data : nextProps.data});
+      this.setState( { data : nextProps.data } );
     }
   }
 
-  getTransform (angle, width) {
-    let x = (0 - width / 2) * Math.cos(angle) - (0 - width / 2) * Math.sin(angle);
-    let y = (0 - width / 2) * Math.sin(angle) + (0 - width / 2) * Math.cos(angle);
+  drawCoordinates (data) {
+    let results = [];
 
-    return [ {translateX: (-1 * x) - width / 2}, {translateY: (-1 * y) + width / 2}, { rotate: angle + 'rad' } ];
+    // Draw the points
+    for (var i = 0; i < data.length - 1; i++) {
+      result = this.drawCoordinate(
+        data[i],
+        data[i+1],
+        this.props.gap,
+        '#FFFFFF00',
+        {borderColor: this.props.primaryColor},
+        false);
+      results.push(result);
+    }
+
+    // Draw the last coordinate
+    var i = data.length-1;
+    result = this.drawCoordinate(
+      data[i],
+      data[i],
+      this.props.gap,
+      '#FFFFFF',
+      {},
+      true);
+    results.push(result);
+
+    // Remove the first point from the last set
+    this.cachedObjs = this.cachedObjs.slice(2);
+    return results;
   }
 
-  drawCoordinate (index, start, end, gap, backgroundColor, lineStyle, lastCoordinate) {
-    let key = 'line' + index;
-    let dx = gap;
-    let dy = end.Y - start.Y;
-    let size = Math.sqrt(dx * dx + dy * dy);
-    let angleRad = -1 * Math.atan2(dy, dx);
-    let height;
-    let top;
+  drawCoordinate (start, end, gap, backgroundColor, lineStyle, isLastCoord) {
+    let dx, dy, size, angle, height, top;
+    // Don't recompute a line that has already been computed from the last set.
+    let idStr = "line-" + start.Id.toString();
+    let isCached = this.cachedObjs.hasOwnProperty(idStr);
+    if (!isCached) {
+      dx = gap;
+      dy = end.Y - start.Y;
+      size = Math.sqrt(dx * dx + dy * dy);
+      angle = -1 * Math.atan2(dy, dx);
+      if (start.Y > end.Y) {
+        height = start.Y;
+        top = -1 * size;
+      }
+      else {
+        height = end.Y;
+        top = -1 * (size - Math.abs(dy));
+      }
+      // Cache this key-value pair.
+      this.cachedObjs[idStr] = { dx: dx, dy: dy, size: size, angle: angle, height: height, top: top };
+    }
+    else {
+      var cachedObj = this.cachedObjs[idStr];
+      dx = cachedObj.dx;
+      dy = cachedObj.dy;
+      size = cachedObj.size;
+      angle = cachedObj.angle;
+      height = cachedObj.height;
+      top = cachedObj.top;
+    }
+
     let topMargin = 20;
 
-    if (start.Y > end.Y) {
-      height = start.Y;
-      top = -1 * size;
-    } else {
-      height = end.Y;
-      top = -1 * (size - Math.abs(dy));
-    }
-
     return (
-      <View key={key} style={{
+      <View key={idStr} style={{
         height: this.props.height + topMargin,
         justifyContent: 'flex-end'
       }}>
@@ -62,17 +101,18 @@ class LineChart extends React.Component {
             top: top,
             width: size,
             height: size,
-            borderColor: lastCoordinate ? backgroundColor : this.props.primaryColor,
+            borderColor: isLastCoord ? backgroundColor : this.props.primaryColor,
             borderTopWidth: 1,
-            transform: this.getTransform(angleRad, size)
-          }, styles.lineBox, lineStyle])} />
+            transform: this.getTransform(idStr, isCached)
+          }, styles.lineBox, lineStyle])} 
+          />
           <View style={StyleSheet.flatten([styles.absolute, {
             height: height - Math.abs(dy) - 2,
-            backgroundColor: lastCoordinate ? '#FFFFFF00' : backgroundColor,
+            backgroundColor: isLastCoord ? '#FFFFFF00' : backgroundColor,
             marginTop: Math.abs(dy) + 2
           }])} />
         </View>
-        {!lastCoordinate ? (
+        {!isLastCoord ? (
           <View style={StyleSheet.flatten([styles.guideLine, {
             width: dx,
             borderRightColor: this.props.xAxisGridLineColor
@@ -82,33 +122,27 @@ class LineChart extends React.Component {
     )
   }
 
-  drawCoordinates (data) {
-    let results = []
-    let dataLength = data.length
-    for (let i = 0; i < dataLength - 1; i++) {
-      let result = this.drawCoordinate(
-        i, 
-        data[i],
-        data[i+1],
-        this.props.gap,
-       '#FFFFFF00', 
-       {borderColor: this.props.primaryColor}, 
-       false);
-      results.push(result);
+  getTransform (idStr, isCached) {
+    let translateX, translateY, angle;
+    let cachedObj = this.cachedObjs[idStr];
+    if (!isCached) {
+      angle = cachedObj.angle;
+      let size = cachedObj.size;
+      let x = (0 - size / 2) * Math.cos(angle) - (0 - size / 2) * Math.sin(angle);
+      let y = (0 - size / 2) * Math.sin(angle) + (0 - size / 2) * Math.cos(angle);
+
+      translateX = (-1 * x) - size / 2;
+      cachedObj.translateX = translateX;
+      translateY = (-1 * y) + size / 2;
+      cachedObj.translateY = translateY;
+    }
+    else {
+      translateX = cachedObj.translateX;
+      translateY = cachedObj.translateY;
+      angle = cachedObj.angle;
     }
 
-    let i = dataLength-1;
-    result = this.drawCoordinate(
-       i, 
-       data[i], 
-       data[i],
-       this.props.gap,
-       '#FFFFFF', 
-       {}, 
-       true);
-    results.push(result);
-    
-    return results;
+    return [ { translateX: translateX }, { translateY: translateY }, { rotate: angle + 'rad' } ];
   }
 
   render () {
@@ -124,7 +158,7 @@ class LineChart extends React.Component {
               height: '100%',
               position: 'relative',
               minWidth: 200}}>
-              {this.drawCoordinates(this.state.data)}
+              { this.drawCoordinates(this.state.data) }
             </View>
           </View>
         </View>
